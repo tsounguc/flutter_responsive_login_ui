0aabcfcd91e6770bad61398b81b6efa98bee26f0
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-13 21:31:33 -0500

    Add outlined buttons

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/widgets/social_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,13 +11,21 @@ class LoginScreen extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              Image.asset("assets/images/signin_balls.phg"),
+              Image.asset("assets/images/signin_balls.png"),
               const Text(
                 "Sign in",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 50,
                 ),
+              ),
+              const SizedBox(height: 50),
+              const SocialButton(iconPath: 'assets/svgs/g_logo.svg', label: 'Continue with Google'),
+              const SizedBox(height: 20),
+              const SocialButton(
+                iconPath: 'assets/svgs/f_logo.svg',
+                label: 'Continue with Facebook',
+                horizontalPadding: 90,
               )
             ],
           ),
